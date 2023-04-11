8fa5a53551f73fdd4e701f12160258e22c6d40d1
Julio-cesaer-hc <julio.cesaer.hc@gmail.com>
2023-04-11 17:54:42 -0600

    agregando el disparo triple utilizando unityEvents

--- Assets/_Scripts/PlayerShooting.cs
+++ Assets/_Scripts/PlayerShooting.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour
 {
-    public GameObject laserPrefab;
+    [SerializeField]
+    private GameObject _laserPrefab;
     // Start is called before the first frame update
     private float _fireRate = 0.12f;
     private float _nextFire = 0.0f;
@@ -16,13 +17,20 @@ public class PlayerShooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Disparo
-        Shooting();
+
     }
 
-    private void Shooting(){
+    public void TripleShooting(){
         if(Input.GetKey(KeyCode.Space) && Time.time > _nextFire){
-            Instantiate(laserPrefab,this.transform.position + new Vector3(0, 0.88f, 0), Quaternion.identity) ;
+            Instantiate(_laserPrefab,this.transform.position + new Vector3(0, 0.88f, 0), Quaternion.identity) ;
+            Instantiate(_laserPrefab,this.transform.position + new Vector3(0.47f, 0.05f, 0), Quaternion.identity) ;
+            Instantiate(_laserPrefab,this.transform.position + new Vector3(-0.47f, 0.05f, 0), Quaternion.identity) ;
+            _nextFire = Time.time + _fireRate;
+        }
+    }
+    public void NormalShooting(){
+        if(Input.GetKey(KeyCode.Space) && Time.time > _nextFire){
+            Instantiate(_laserPrefab,this.transform.position + new Vector3(0, 0.88f, 0), Quaternion.identity) ;
             _nextFire = Time.time + _fireRate;
         }
     }
